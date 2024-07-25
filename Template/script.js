7780949cfce23e79b84d9f462361a6f528ab2487
hadiobeid <hadi.h.obeid@outlook.com>
2024-07-25 19:40:16 +0100

    Adding initial calendar html template

--- Template/script.js
+++ Template/script.js
@@ -1,11 +1,12 @@
-let container = document.querySelector('.container');
 
-calender.draw({
-    target:'#dycalendar',
-    dayformat:'full',
-    type:'month',
-    monthformat:'full',
-    heighlighttoday: true,
-    prevnextbutton: 'show'
-});
 
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    
+    calendar.render();
+    let _startdate = new Date('01/01/2025')
+    calendar.gotoDate(_startdate)
+  });
