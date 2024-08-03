7d58641b5a7f039b6a756617866862827044e23a
hadiobeid <hadi.h.obeid@outlook.com>
2024-08-03 16:52:33 +0100

    Added the ability to allow the user to select the dates they want off

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ def get_weekend_list_days(weekend_str: str):
             weekend_days.append(day)
     return weekend_days
 
+def get_selected_dates_off(selected_dates_off: str):
+    if selected_dates_off == 'None':
+        return []
+    return selected_dates_off.split(',')
+    
+
 @app.route('/', methods =["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -28,6 +34,7 @@ def home():
         year = int(request.form.get("selected_year"))
         country = request.form.get("selected_country")
         state = request.form.get("states_select") if request.form.get("states_select") else 'All'
+        _selected_days_off = request.form.get("selected_off") if request.form.get("selected_off") != '' else 'None'
         _max_workdays = request.form.get("max_days")
         _min_holidays = request.form.get("min_holidays")
         _weekend_days = [request.form.get("Monday"), request.form.get("Tuesday"), request.form.get("Wednesday"),
@@ -36,19 +43,21 @@ def home():
         
         return redirect(url_for('calendar_view',holidays_number= holidays_number,
                                 year= year, country=country, state=state,
-                                max_workdays=_max_workdays, min_holidays=_min_holidays, weekend_days=_weekend_days))
+                                max_workdays=_max_workdays, min_holidays=_min_holidays, weekend_days=_weekend_days, 
+                                selected_days_off = _selected_days_off))
     else:
         return render_template('index.html', country_states = cs.countries_state_dict)
 
 
-@app.route('/CalendarView/<holidays_number>/<year>/<country>/<state>/<max_workdays>/<min_holidays>/<weekend_days>')
-def calendar_view(holidays_number, year, country, state, max_workdays, min_holidays, weekend_days):
+@app.route('/CalendarView/<holidays_number>/<year>/<country>/<state>/<max_workdays>/<min_holidays>/<weekend_days>/<selected_days_off>')
+def calendar_view(holidays_number, year, country, state, max_workdays, min_holidays, weekend_days, selected_days_off):
     _year= int(year)
     max_workdays = int(max_workdays)
     min_holidays = int(min_holidays)
     state = state if state != 'All' else ''
     weekend_days = get_weekend_list_days(weekend_days)
-    holiday_planner = HolidayPlanner(int(holidays_number),country, state, int(_year), max_workdays, min_holidays, weekend_days)
+    selected_days_off = get_selected_dates_off(selected_days_off)
+    holiday_planner = HolidayPlanner(int(holidays_number),country, state, int(_year), max_workdays, min_holidays, weekend_days, selected_days_off)
     holidays, holiday_types, remaining_days = holiday_planner.get_holiday_plan()
     return render_template('calendar_view.html', 
                            holidays = holidays, remaining_days = remaining_days,
